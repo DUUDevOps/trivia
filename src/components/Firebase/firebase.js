e9d405db709ea8ae395c0e093ca9d12191e84676
Dylan Powers <ddp19@duke.edu>
2019-10-21 18:33:24 -0400

    Added boilerplate frontend for Admin login

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -2,20 +2,41 @@ import firebase from 'firebase';
 import React from 'react';
 
 var config = {
-	apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: "devops-trivia-18a2c.firebaseapp.com",
-    databaseURL: "https://devops-trivia-18a2c.firebaseio.com",
-    projectId: "devops-trivia-18a2c",
-    storageBucket: "",
-    messagingSenderId: "701353522538",
-    appId: "1:701353522538:web:e621e9c40ab4fbf2dee113",
-    measurementId: "G-SJ6TD2BJRW"
+	apiKey: "AIzaSyAwyhl8Pi7drd3ls6bSaxWTYx3LnyyO7_Q",
+  authDomain: "devops-trivia-18a2c.firebaseapp.com",
+  databaseURL: "https://devops-trivia-18a2c.firebaseio.com",
+  projectId: "devops-trivia-18a2c",
+  storageBucket: "",
+  messagingSenderId: "701353522538",
+  appId: "1:701353522538:web:e621e9c40ab4fbf2dee113",
+  measurementId: "G-SJ6TD2BJRW"
 };
 
+const DUU_ADMIN_USERNAME = "duu-admin";
+
 class Firebase {
-    constructor() {
-        firebase.initializeApp(config)
+  constructor() {
+    firebase.initializeApp(config);
+    this.auth = firebase.auth();
+  }
+
+  signInWithEmail = (email, password, callback) => {
+    if (email !== DUU_ADMIN_USERNAME) {
+      callback({
+        message: "Incorrect Admin Account",
+        status: 500,
+      });
     }
+
+    this.auth.signInWithEmailAndPassword(email, password)
+      .then(() => callback(null))
+      .catch((error) => {
+        callback({
+          message: error.message,
+          status: 500
+        });
+      });
+  };
 }
 
 const FirebaseContext = React.createContext(null);
