63c98a31ed68316f62d45135bf8ba6cda34f0094
Dylan Powers <ddp19@duke.edu>
2019-10-28 19:37:00 -0400

    Added behavior for signing in, transitioning to quiz create page

---
+++ src/containers/admin/CreateQuizPage/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import styles from '../../client/RegisterPage/styles.module.css';
+import TextInput from '../../../components/TextInput';
+
+class CreateQuizPage extends React.Component {
+  render() {
+    return (
+      <h1>
+        This is the create quiz page
+      </h1>
+    );
+  }
+}
+
+export default CreateQuizPage;
