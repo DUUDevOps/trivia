a0f0070c62102970c14a4c93953046da3d82afb0
Dylan Powers <ddp19@duke.edu>
2019-11-05 18:51:46 -0500

    Added state for CreateQuizPage

--- src/containers/admin/CreateQuizPage/index.js
+++ src/containers/admin/CreateQuizPage/index.js
@@ -3,7 +3,25 @@ import React from 'react';
 import styles from '../../client/RegisterPage/styles.module.css';
 import TextInput from '../../../components/TextInput';
 
+const NUM_QUESTIONS = 10;
+
 class CreateQuizPage extends React.Component {
+
+  constructor() {
+
+    var defaultQA = [];
+    for (let i = 0; i < NUM_QUESTIONS; i += 1) {
+      defaultQA.push({
+        question: '',
+        answer: ''
+      });
+    }
+
+    this.state = {
+      questionsAndAnswers: defaultQA,
+    }
+  }
+
   render() {
     return (
       <h1>
