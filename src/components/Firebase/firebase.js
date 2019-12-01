386fe1966de8118f6aca0207590dd8e8367b11ce
Dylan Powers <ddp19@duke.edu>
2019-11-30 23:33:56 -0500

    Added functionality for retreiving user questions

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -11,6 +11,7 @@ const config = {
   appId: "1:701353522538:web:e621e9c40ab4fbf2dee113",
   measurementId: "G-SJ6TD2BJRW"
 };
+
 const ADMIN_REF_NAME = "quizzes";
 const NUM_QUESTIONS = 10;
 
@@ -20,6 +21,11 @@ class Firebase {
     this.auth = firebase.auth();
   }
  
+  /**
+   * @param {string} email
+   * @param {string} password
+   * @param {function} callback
+   */
   signInWithEmail = (email, password, callback) => {
     this.auth.signInWithEmailAndPassword(email, password)
       .then(() => callback(null))
@@ -28,17 +34,94 @@ class Firebase {
       });
   };
 
+  /**
+   * @param {Object} quiz
+   * @param {function} callback
+   */
   saveQuizFromAdmin = (quiz, callback) => {
-    let quizRef = firebase.database().ref().child(this.getCurrentFormattedDate());
+    let quizRef = firebase.database().ref().child(this.getCurrentFormattedDate()).child('admin');
 
     // firebase doesn't allow insertion of arrays, so create children by auto id
+    var questionNum = 1;
     quiz.forEach(QASet => {
-      quizRef.push().set(QASet);
+      quizRef.child(questionNum).set(QASet);
+      questionNum += 1;
     });
 
     callback();
   };
 
+  /**
+   * @param {number} team
+   * @param {array} answers
+   * @param {function} callback
+   */
+  saveAnswersForTeam = (team, answers, callback) => {
+    let teamRef = firebase.database().ref()
+      .child(this.getCurrentFormattedDate())
+      .child('teams')
+      .child(String(teamNumber));
+
+    answers.forEach((answer, index) => {
+      teamRef.child(index).set(answer);
+    });
+
+    callback();
+  };
+
+  /**
+   * @param {Number} teamNumber
+   * @param {function} callback
+   */
+  getAnswersForTeam = (teamNumber, callback) => {
+    let teamRef = firebase.database().ref()
+      .child(this.getCurrentFormattedDate())
+      .child('teams')
+      .child(String(teamNumber));
+
+    teamRef.once('value')
+      .then(snapshot => {
+        if (!snapshot.exists()) {
+          return;
+        }
+
+        var answers = new Array(snapshot.numChildren());
+
+        snapshot.forEach(answer => {
+          answers[parseInt(answer.key)] = answer.val();
+        });
+
+        callback(answers);
+      }).catch(error => console.log(error));
+  }
+
+  /**
+   * @param {function} callback
+   */
+  getQuestionsAndCorrectAnswers = (callback) => {
+    let roundRef = firebase.database().ref()
+      .child(this.getCurrentFormattedDate())
+      .child('admin');
+
+    roundRef.once('value')
+      .then(snapshot => {
+        if (!snapshot.exists()) {
+          return;
+        }
+
+        var questionsAndAnswers = new Array(snapshot.numChildren())
+
+        snapshot.forEach(question => {
+          questionsAndAnswers[parseInt(question.key)] = question.val();
+        });
+
+        callback(questionsAndAnswers);
+      }).catch(error => console.log(error));
+  }
+
+  /**
+   * @returns {string}
+   */
   getCurrentFormattedDate = () => {
     // this will return the date in yyyy-mm-dd
     return new Date().toISOString().slice(0, 10);
@@ -46,6 +129,12 @@ class Firebase {
 }
 
 const FirebaseContext = React.createContext(null);
+
+export const withFirebase = Component => props => (
+  <FirebaseContext.Consumer>
+    {firebase => <Component {...props} firebase={firebase} />}
+  </FirebaseContext.Consumer>
+);
 
 export default Firebase;
 
