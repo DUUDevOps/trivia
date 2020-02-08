ceac6ce918a437a5bd9d8dce961aad02360737ee
Dylan Powers <ddp19@duke.edu>
2020-02-08 14:01:58 -0500

    Updates

---
+++ precache-manifest.f3b719e5888195b94c35a5d39e1fb3ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83b947a5f996d31c9f0092547c78ea7e",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "37975f9f3608d806e689",
+    "url": "/trivia/static/css/main.6bbc33a0.chunk.css"
+  },
+  {
+    "revision": "0c0618a0363a3fcd5dd7",
+    "url": "/trivia/static/js/2.4d6f66da.chunk.js"
+  },
+  {
+    "revision": "a63b2a53d595f2f6f0afe561bb0fa89d",
+    "url": "/trivia/static/js/2.4d6f66da.chunk.js.LICENSE"
+  },
+  {
+    "revision": "37975f9f3608d806e689",
+    "url": "/trivia/static/js/main.00faf3b6.chunk.js"
+  },
+  {
+    "revision": "1f27a0ea9b331ddfb05a",
+    "url": "/trivia/static/js/runtime-main.e7fbfa86.js"
+  },
+  {
+    "revision": "b3375bd4f0eafc77f68b0818592149e6",
+    "url": "/trivia/static/media/DukeNiteLogo.b3375bd4.png"
+  }
+]);
